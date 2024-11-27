f044747802b8a7445d04c86d43dd5402b0e1d357
dinnosauro <rique.almeida.santos@hotmail.com>
2024-11-27 18:49:33 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log ("Olá mundo")
+let nome=Dinno
+let Xp=2020
+switch (Xp){
+    case "Ferro" Se XP for menor do que 1.000 = Ferro
+    break;
+    case "Bronze" Se XP for entre 1.001 e 2.000 = Bronze
+    break;
+    case "Prata" Se XP for entre 2.001 e 5.000 = Prata
+    break;
+    case "Ouro" Se XP for entre 5.001 e 7.000 = Ouro
+    break;
+    case "Platina" Se XP for entre 7.001 e 8.000 = Platina
+    break;
+    case "Ascendente" Se XP for entre 8.001 e 9.000 = Ascendente
+    break;
+    case "Imortal" Se XP for entre 9.001 e 10.000= Imortal
+    break;
+    case "Radiante" Se XP for maior ou igual a 10.001 = Radiante
+    break;
+}
+
+console.log ("O herói " + nome "está no nível de " + Nv)
