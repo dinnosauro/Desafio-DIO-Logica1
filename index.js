63eb853bd429d6ec4df37b49c9beb678442e6056
dinnosauro <rique.almeida.santos@hotmail.com>
2024-12-03 14:04:40 -0300

    teste

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-let nome=Dinno
-let Xp=2020
-switch (Xp){
-    case "Ferro" Se XP for menor do que 1.000 = Ferro
+let nome="Dinno"
+let xp="Prata"
+switch (xp){
+    case "Ferro":
+        console.log("O herói " + nome, "está no nível de " + xp)    
+    // Se XP for menor do que 1.000 = Ferro
     break;
-    case "Bronze" Se XP for entre 1.001 e 2.000 = Bronze
+    case "Bronze":
+        console.log("O herói " + nome, "está no nível de " + xp)
+    // Se XP for entre 1.001 e 2.000 = Bronze
     break;
-    case "Prata" Se XP for entre 2.001 e 5.000 = Prata
+    case "Prata":
+    console.log("O herói " + nome, "está no nível de " + xp)
+    // Se XP for entre 2.001 e 5.000 = Prata
     break;
-    case "Ouro" Se XP for entre 5.001 e 7.000 = Ouro
+    case "Ouro":
+        console.log("O herói " + nome, "está no nível de " + xp)
+        //Se XP for entre 5.001 e 7.000 = Ouro
     break;
-    case "Platina" Se XP for entre 7.001 e 8.000 = Platina
+    case "Platina": 
+        console.log("O herói " + nome, "está no nível de " + xp)
+        //Se XP for entre 7.001 e 8.000 = Platina
     break;
-    case "Ascendente" Se XP for entre 8.001 e 9.000 = Ascendente
+    case "Ascendente": 
+        console.log("O herói " + nome, "está no nível de " + xp)
+        //Se XP for entre 8.001 e 9.000 = Ascendente
     break;
-    case "Imortal" Se XP for entre 9.001 e 10.000= Imortal
+    case "Imortal":
+        console.log("O herói " + nome, "está no nível de " + xp)
+        // Se XP for entre 9.001 e 10.000= Imortal
     break;
-    case "Radiante" Se XP for maior ou igual a 10.001 = Radiante
+    case "Radiante":
+        console.log("O herói " + nome, "está no nível de " + xp)
+        // Se XP for maior ou igual a 10.001 = Radiante
     break;
 }
 
-console.log ("O herói " + nome "está no nível de " + Nv)
+//console.log ("O herói " + nome "está no nível de " + XP)
